de4df2e0976d99c905c98015d2507c9747bdc837
Raul Torres <55553058+raultorres2603@users.noreply.github.com>
2026-03-22 17:45:30 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_DEPLOYMENT_ENV === 'production' ? '/' : '/complai-frontend-dev/',
+  base: process.env.VITE_DEPLOYMENT_ENV === 'production' ? '/complai-frontend/' : '/complai-frontend/complai-frontend-dev/',
   plugins: [react()],
   server: {
     port: 5173,
